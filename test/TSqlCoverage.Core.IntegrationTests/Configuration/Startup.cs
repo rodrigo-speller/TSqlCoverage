338b5bde7d6cdbdb9897e26912df07c760e2336a
Rodrigo Speller <rodrigo@primosti.com.br>
2020-08-23 23:44:04 -0300

    Configuration support to integrantion tests

--- test/TSqlCoverage.Core.IntegrationTests/Configuration/Startup.cs
+++ test/TSqlCoverage.Core.IntegrationTests/Configuration/Startup.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Rodrigo Speller. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TSqlCoverage.Core.IntegrationTests.Configuration
@@ -9,7 +10,15 @@ namespace TSqlCoverage.Core.IntegrationTests.Configuration
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<Startup>();
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddJsonFile(".private/appsettings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables("TSQLCOVERAGE_TEST_")
+                ;
+
+            var configuration = configurationBuilder.Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
         }
     }
 }
